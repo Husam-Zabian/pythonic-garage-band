8a19f7cd3c487f4ec9a324b2eb9615005a9e1089
Husam-Zabian <husamthubian99@yahoo.com>
2023-04-12 11:09:25 +0000

    Lab04 Done

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pythonic-garage-band
+
+## Lab - Class04
+
+## Author Name : Husam Zabian
+
+## Summary
+
+*we used the pytest in order to get the all objects inside the class to be recieved successfully and we used the inheritance to get some function features from our parent classes*
---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,99 @@
+from abc import ABC, abstractmethod
+
+class Musician: 
+   
+    def __init__(self ,name) :
+        self.name = name
+        
+   
+    def __str__(self):
+        return f'My name is {self.name} and I play guitar'
+    
+    def __repr__(self): 
+        return f'Guitarist instance. Name = {self.name}'
+    def get_instrument(self): 
+        return 'guitar'
+    
+    @abstractmethod 
+    def play_solo(self):
+        pass
+
+class Guitarist(Musician): 
+   
+    def __init__(self ,name) :
+        self.name = name
+        
+   
+    def __str__(self):
+        return f'My name is {self.name} and I play guitar'
+    
+    def __repr__(self): 
+        return f'Guitarist instance. Name = {self.name}'
+    def get_instrument(self): 
+        return 'guitar'
+    
+    def play_solo(self):
+        return 'face melting guitar solo'
+    
+class Drummer(Musician): 
+    def __init__(self ,name) :
+        self.name = name
+        
+   
+    def __str__(self):
+        return f'My name is {self.name} and I play drums'
+    
+    def __repr__(self): 
+        return f'Drummer instance. Name = {self.name}' 
+    
+    def get_instrument(self): 
+        return 'drums'
+    
+    def play_solo(self):
+        return 'rattle boom crash'
+    
+class Bassist(Musician): 
+    def __init__(self ,name) :
+        self.name = name
+        
+   
+    def __str__(self):
+        return f'My name is {self.name} and I play bass'
+    
+    def __repr__(self): 
+        return f'Bassist instance. Name = {self.name}'
+    
+    def get_instrument(self): 
+        return 'bass'
+    
+    def play_solo(self):
+        return 'bom bom buh bom'
+    
+
+class Band: 
+    instances = []
+
+    def __init__(self,name,members) :
+        Band.instances.append(self)
+        self.name = name
+        self.members = members
+        
+ 
+    def play_solos(self):
+      solos=[]
+      for i in self.members:
+       solos.append(i.play_solo())
+      return solos 
+   
+    def __str__(self):
+        return f'our team is {self.name} and we play Rock music'
+    
+    def __repr__(self): 
+        return f'The Memory Remains. BandName = {self.name}'
+
+    def to_list():
+      
+       return Band.instances
+            
+  
+             
